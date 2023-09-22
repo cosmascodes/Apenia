204bfc7c2848c6a018d0cff23a8bcaaca0350451
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2023-09-22 13:21:30 +0300

    thanks

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 
 function AboutUs() {
   return (
-    <div>
+    <>
     <section
        className="bg-[#f9f9f9] bg-cover
        font-[Mulish] md:bg-top bg-center sticky top-0 z-50"
      >
        <Navbar/>
      </section>
-    </div>
+      <section className="mt-14 mb-14 px-2 md:px-20 border-t border-t-slate-200">
+        <Footer/>
+      </section>
+    </>
   )
 }
 
