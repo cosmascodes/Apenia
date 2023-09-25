ad31aba1c16a1e878eee9f9a15da6a8770894d9d
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2023-09-25 12:20:52 +0300

    thanks

--- src/components/Somecontent/content.jsx
+++ src/components/Somecontent/content.jsx
@@ -23,7 +23,7 @@ function Content() {
               <h1 className='text-[32px] md:pr-40 text-primary leading-[43px] font-[900]'>
                 Borderless Cash Transfer.
               </h1>
-              <p className='mb-5 mt-5 md:w-[315px] text-[14px]'>
+              <p className='mb-5 mt-5 text-[14px]'>
                 Cash transfer solutions with a borderless capability allow individuals or businesses to send and receive money across international boundaries seamlessly.
                 These services typically use digital platforms and can be cost-effective compared to traditional banking wire transfers.
               </p>
@@ -41,7 +41,7 @@ function Content() {
               <h1 className='text-[32px] md:pr-40 text-primary leading-[43px] font-[900]'>
                 Personal Budgeting and Spending.
               </h1>
-              <p className='mb-5 mt-5 md:w-[315px] text-[14px]'>
+              <p className='mb-5 mt-5 text-[14px]'>
                 Personal budgeting involves creating a financial plan that outlines income, expenses, and savings goals.
                 Modern budgeting tools and apps make it easy for individuals to track their spending, categorize expenses, and set budget limits.
                 Effective personal budgeting promotes financial discipline, helps individuals reach their financial objectives, and ensures better money management.
@@ -60,7 +60,7 @@ function Content() {
               <h1 className='text-[32px] md:pr-40 text-primary leading-[43px] font-[900]'>
                 Saving account.
               </h1>
-              <p className='mb-5 mt-5 md:w-[315px] text-[14px]'>
+              <p className='mb-5 mt-5 text-[14px]'>
                 Saving money involves setting aside a portion of income for future needs or emergencies.
                 Savings can be kept in various financial instruments like savings accounts and investment accounts.
                 Saving is a fundamental financial practice that provides financial security and enables individuals to achieve their long-term goals.SAVE & EARN Earn up to 12%* interest a year
@@ -80,10 +80,48 @@ function Content() {
               <h1 className='text-[32px] md:pr-40 text-primary leading-[43px] font-[900]'>
                 Investments.
               </h1>
-              <p className='mb-5 mt-5 md:w-[315px] text-[14px]'>
+              <p className='mb-5 mt-5 text-[14px]'>
                 Investment involves using saved money to generate returns over time, typically by purchasing assets such as stocks, bonds, real estate, or mutual funds.
                 Investments can offer the potential for wealth growth and financial security but also come with varying levels of risk.
                 Diversifying an investment portfolio is a strategy to manage risk while seeking financial growth.
+              </p>
+            </div>
+          </Slide>
+        </div>
+        <div className={`flex mt-40 gap-10 justify-between flex-col-reverse md:flex-row-reverse`}>
+          <Slide direction="up" triggerOnce delay={2} cascade damping={1.5} className='md:w-1/2'>
+            <div className='md:w-full select-none relative flex justify-center items-center'>
+              <img src={image4} alt="logo" className="md:cursor-arrow select-none h-60" />
+            </div>
+          </Slide>
+          <Slide direction="up" triggerOnce delay={2} cascade damping={1.5} className='md:w-1/2'>
+            <div className='md:w-full font-[Mulish] text-center md:text-left'>
+              <h1 className='text-[32px] md:pr-40 text-primary leading-[43px] font-[900]'>
+                Global Payment(Virtual and Physical Card).
+              </h1>
+              <p className='mb-5 mt-5 text-[14px]'>
+              Global payment solutions enable individuals and businesses to make payments and transactions across international borders.
+              These services often offer currency conversion, allowing users to pay or receive funds in their preferred currency.
+              Global payment platforms simplify cross-border commerce, supporting international trade and financial interactions.
+              </p>
+            </div>
+          </Slide>
+        </div>
+        <div className={`flex mt-40 gap-10 justify-between flex-col-reverse md:flex-row`}>
+          <Slide direction="up" triggerOnce delay={2} cascade damping={1.5} className='md:w-1/2'>
+            <div className='md:w-full select-none relative flex justify-center items-center'>
+              <img src={image5} alt="logo" className="md:cursor-arrow select-none h-60" />
+            </div>
+          </Slide>
+          <Slide direction="up" triggerOnce delay={2} cascade damping={1.5} className='md:w-1/2'>
+            <div className='md:full font-[Mulish] text-center md:text-left'>
+              <h1 className='text-[32px] md:pr-40 text-primary leading-[43px] font-[900]'>
+                Multi-currency.
+              </h1>
+              <p className='mb-5 mt-5 text-[14px]'>
+                Currency conversion services allow individuals or businesses to exchange one currency for another at current exchange rates.
+                This is essential for international travel, trade, and investment, ensuring transactions can occur seamlessly across different currencies.
+                Currency conversion services may be offered by banks, currency exchange bureaus, or online platforms.
               </p>
             </div>
           </Slide>
