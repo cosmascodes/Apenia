06d51169b4f1ac3e25d20a14cacb0dd96310e3de
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-03-19 11:59:19 +0300

    push

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,34 +11,38 @@ const Navbar = () => {
         <div className="z-50 px-5 py-4 md:w-auto w-full flex justify-between">
           <Link to="/">
             <img src={Logo} alt="logo" className="md:cursor-pointer h-9" />
-          </Link> 
+          </Link>
           <div className="text-3xl md:hidden" onClick={() => setOpen(!open)}>
             <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
           </div>
         </div>
-        <ul className="md:flex text-[12px] hidden items-center gap-1 font-[Mulish] font-[700]">
+        <ul className="md:flex text-sm md:text-base hidden items-center gap-1 font-[Mulish] font-[700]">
           <NavLinks />
           <li className="px-3">
             <Link to="/invest">Invest in Business</Link>
           </li>
         </ul>
         <div className="md:block hidden">
-          <div className="flex text-[12px] font-[700] items-center gap-4">
+          <div className="flex text-sm sm:text-base font-[700] items-center gap-4">
             <Link to="http://waitwhile.com/locations/apenia">Sign in</Link>
             <Link to="/join">
-              <Button name="Join Apenia"/>
+              <Button name="Join Apenia" />
             </Link>
           </div>
         </div>
         {/* Mobile nav */}
-        <ul className={`md:hidden bg-white fixed w-full h-screen top-0 overflow-y-auto bottom-0 py-24 pl-4 ${open ? "left-0" : "left-[-100%]"} duration-200`}>
+        <ul
+          className={`md:hidden bg-white fixed w-full h-screen top-0 overflow-y-auto bottom-0 py-24 pl-4 ${
+            open ? "left-0" : "left-[-100%]"
+          } duration-200`}
+        >
           <NavLinks />
           <li className="px-3">
             <Link to="/invest">Invest in Business</Link>
           </li>
           <div className="py-5">
             <Link to="/join">
-              <Button name="Join Apenia"/>
+              <Button name="Join Apenia" />
             </Link>
           </div>
         </ul>
--- src/pages/invest.jsx
+++ src/pages/invest.jsx
@@ -78,13 +78,13 @@ function Invest() {
                 <h1 className="text-[26px] text-primary leading-[43px] font-[900]">
                   Become an Investor
                 </h1>
-                <p className="mb-5 mt-5 text-[12px] md:text-[14px]">
-                  Unlock the power of passive income with Apenia Bank, where
-                  your investments grow effortlessly without the need for
-                  constant oversight. Our user-friendly platform offers
+                <p className="mb-5 mt-5 text-sm md:text-base">
+                  Tap into the potential of passive income with Apenia Bank,
+                  where AI-driven investments grow effortlessly, eliminating the
+                  need for constant oversight. Our intuitive platform ensures
                   attractive returns, flexible withdrawals, and unwavering
-                  transparency, making it the ideal choice for those seeking a
-                  hassle-free and profitable investment experience
+                  transparency, making it the perfect choice for those seeking a
+                  hassle-free and profitable investment experience.
                 </p>
               </div>
             </Slide>
@@ -120,13 +120,12 @@ function Invest() {
                 <h1 className="text-[26px] text-primary leading-[43px] font-[900]">
                   Passive income opportunity
                 </h1>
-                <p className="mb-5 mt-5 text-[12px] md:text-[14px]">
-                  By investing with Apenia, you can earn passive income without
-                  having to actively manage your investments. You can choose the
-                  amount you want to invest and the duration of your investment,
-                  and WeLend will take care of the rest. With Apenia, you can
-                  earn attractive returns while supporting borrowers who need
-                  financing to achieve their goals.
+                <p className="mb-5 mt-5 text-sm md:text-base">
+                  Invest with Apenia and earn passive income without the need
+                  for active management. Choose your investment amount and
+                  duration, and let our AI-driven platform handle the rest. With
+                  Apenia, enjoy attractive returns while supporting borrowers in
+                  achieving their goals.
                 </p>
               </div>
             </Slide>
@@ -162,14 +161,11 @@ function Invest() {
                 <h1 className="text-[26px] text-primary leading-[43px] font-[900]">
                   Withdrawable principal investment
                 </h1>
-                <p className="mb-5 mt-5 text-[12px] md:text-[14px]">
-                  One of the key benefits of investing with Apenia is that the
-                  principal investment is withdrawable anytime upon request. We
-                  understand that circumstances can change, and you may need
-                  access to your investment funds. Apenia offers the flexibility
-                  to withdraw your principal investment at any time, without any
-                  penalty or fees. This provides peace of mind to investors,
-                  knowing that their funds are not locked in for a fixed term.
+                <p className="mb-5 mt-5 text-sm md:text-base">
+                  Apenia offers the flexibility of withdrawing your principal
+                  investment at any time, without penalties. We understand
+                  circumstances may change, providing peace of mind to
+                  investors, knowing their funds aren't locked in.
                 </p>
               </div>
             </Slide>
@@ -205,7 +201,7 @@ function Invest() {
                 <h1 className="text-[26px] text-primary leading-[43px] font-[900]">
                   Guaranteed investment transparency
                 </h1>
-                <p className="mb-5 mt-5 text-[12px] md:text-[14px]">
+                <p className="mb-5 mt-5 text-sm md:text-base">
                   In addition, Apenia guarantees transparency on investments. We
                   provide regular updates and reports on the performance of your
                   investment, including the interest earned and any fees
@@ -223,7 +219,7 @@ function Invest() {
       <section className="px-5 md:px-20">
         <JoinCard />
       </section>
-      <section className="mt-14 mb-14 px-2 md:px-20 border-t border-t-slate-200">
+      <section className="mt-14 border-t border-t-slate-200">
         <Footer />
       </section>
     </>
